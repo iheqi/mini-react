8c6d9d17473b0cee341bc38bdc82c1ba5098cbb8
何琦 <>
2023-04-01 17:01:21 +0800

    chore: mini-react-demo

---
+++ mini-react-demo/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const jsx = <div>hello <span>mini react</span></div>
+
+
+console.log(React);
+console.log(ReactDOM);
+console.log(jsx);

994c608ba6b56ee06c7e1ba51355069f278da0f7
何琦 <>
2023-04-02 21:35:06 +0800

    test: 删除替换操作

--- demos/test-fc/main.tsx
+++ demos/test-fc/main.tsx
@@ -4,15 +4,12 @@ import ReactDOM from 'react-dom/client';
 function App() {
 	const [num, setNum] = useState(100);
 	window.setNum = setNum;
-	return <span>{num}</span>;
+	return num === 3 ? <Child /> : <span>{num}</span>;
 }
 
-const jsx = (
-	<div>
-		<App />
-	</div>
-);
-
+function Child() {
+	return <div>mini react</div>;
+}
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<App />
 );
